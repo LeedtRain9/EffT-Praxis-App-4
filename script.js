241c908fc676ef0f1a1845b3820691a8572ec977
LeedtRain9 <hendrick.neef@gmx.de>
2025-04-07 20:25:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -590,7 +590,7 @@ function displayOverallScore() {
 
     // Sound-Logik (nur bei Platin) - OHNE AUTOPLAY
     if (medal === 'Platin') {
-        const platinSound = new Audio('Die Flippers.mp3');
+        const platinSound = new Audio('Aber Bitte Mit Sahne.mp3');
         
         // Sound-Button
         const soundButton = document.createElement('button');
